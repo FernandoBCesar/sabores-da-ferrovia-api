08a1cb5fce1a1315afce15b003de8bc7050d9ee1
Fernando Benati Cesar Vellozo <fernando16012@gmail.com>
2025-06-12 19:56:15 -0300

    feat: removi algumas linhas

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,3 +1,18 @@
 const express = require("express");
-
 const router = express.Router();
+
+const TaskController = require('../controllers/TaskController');
+
+// Rota para criar uma nova tarefa
+router.post('/tasks', TaskController.create);
+
+// Rota para listar todas as tarefas
+router.get('/tasks', TaskController.findAll);
+
+// Rota para atualizar uma tarefa
+router.put('/tasks/:id', TaskController.update);
+
+// Rota para deletar uma tarefa
+router.delete('/tasks/:id', TaskController.delete);
+
+module.exports = router;
