c02490d88a7e4768674f43481fc1b854b66d64cd
Fernando Benati Cesar Vellozo <fernando16012@gmail.com>
2025-06-13 20:03:42 -0300

    fix: resolvi bugs

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -8,18 +8,5 @@ module.exports = {
         return res.status(200).json(tasks);
   },
 
-  async createTask(req, res) {
-    const { title, description } = req.body;
-
-    if (!title || !description) {
-      return res
-        .status(400)
-        .json({ message: "Título e descrição são obrigatórios" });
-    }
-
-    const newTask = taskRepository.create({ title, description });
-    await taskRepository.save(newTask);
-
-    return res.status(201).json(newTask);
-  },
-};
+  
+}
