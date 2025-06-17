b40f8fd98779ffb2b07f0a84ea281e6b47402c96
Fernando Benati Cesar Vellozo <fernando16012@gmail.com>
2025-06-17 19:56:20 -0300

    feat:nova database

--- src/database/data-source.js
+++ src/database/data-source.js
@@ -1,11 +1,13 @@
 const { DataSource } = require("typeorm");
 const User = require("../entities/User");
+const restaurant = require("../entities/restaurant");
+const favorites = require("../entities/favorites");
 
 const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/database.sqlite",
   synchronize: true,
-  entities: [User],
+  entities: [User, restaurant, favorites],
 });
 
 module.exports = { AppDataSource };
