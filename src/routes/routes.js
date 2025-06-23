92560e529f149731ac07823f210403fa2bdfb476
Fernando Benati Cesar Vellozo <fernando16012@gmail.com>
2025-06-23 17:18:41 -0300

    feat: criado os novos controles

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const AuthController = require("../controllers/auth.controller");
+const RestaurantController = require("../controllers/restaurant.controller");
+const ReviewController = require("../controllers/review.controller");
+
 const routes = express.Router();
 
+//autenticacao
 routes.post("/auth/register", AuthController.register);
 routes.post("/auth/login", AuthController.login);
 
